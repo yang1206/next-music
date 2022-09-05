17760a144996180552629c942f44110d894a33c2
yang1206 <yang15188280673@outlook.com>
2022-09-05 08:37:36 +0800

    fix: 🧩 修复样式问题

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,3 @@
-import { RouteObject } from '@/routers/interface'
 import { address } from '@/common/localData'
 /**
  * @description 播放量转发
@@ -146,24 +145,6 @@ export function parseTime(time: string | number | Date | any, cFormat: string): 
   })
   // eslint-disable-next-line camelcase
   return time_str
-}
-
-/**
- * @description 递归查询对应的路由
- * @param {String} path 当前访问地址
- * @param {Array} routes 路由列表
- * @returns array
- */
-export const searchRoute = (path: string, routes: RouteObject[] = []): RouteObject => {
-  let result: RouteObject = {}
-  for (let item of routes) {
-    if (item.path === path) return item
-    if (item.children) {
-      const res = searchRoute(path, item.children)
-      if (Object.keys(res).length) result = res
-    }
-  }
-  return result
 }
 
 /**
